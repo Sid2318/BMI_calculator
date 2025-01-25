9402f30ff5a964e44925fe17f9cc692a1fedcfb7
Sid2318 <moholsiddhi18@gmail.com>
2025-01-25 20:27:04 +0530

    25/1/25

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //use states
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+
+  let calcBmi = (e) => {
+    //prevent submitting to the server
+    e.preventDefault()
+    if (weight === 0 || height === 0) {
+      alert('Please enter a valid weight and height')
+    } else {
+      let bmi = (weight / (height * height) * 703)
+      setBmi(bmi.toFixed(1))
+      //.toFixed(1) method ensures that the bmi is rounded to 1 decimal place.
+ 
+      // Logic for message
+ 
+      if (bmi < 25) {
+        setMessage('You are underweight')
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
+    }
+  }
+
+  let reload = () => {
+    window.location.reload()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BMI Calculator</h1>
+      <form onSubmit={calcBmi}>
+        <label>Weight (ibs)</label>
+        <input type="number"  value = {weight}  onChange={(e) => setWeight(e.target.value)} ></input>
+        <label>Height (inch)</label>
+        <input type="number"  value = {height}  onChange={(e) => setHeight(e.target.value)} ></input>
+        <div>
+          <button type='submit'>Submit</button>
+          <button onClick={reload} type='submit'>Reload</button>
+        </div>
+        <p className='final'>Your BMI is: {bmi}</p>
+        <p>{message}</p>
+      </form>
     </div>
   );
 }
